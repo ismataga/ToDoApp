63d54d9c19c9ff072b9a057d4d71710279d8c9eb
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-06-25 05:27:41 +0400

    add ToDoRepository

--- src/main/java/com/ismataga/to_do_app/repository/ToDoRepository.java
+++ src/main/java/com/ismataga/to_do_app/repository/ToDoRepository.java
@@ -1,10 +1,10 @@
 package com.ismataga.to_do_app.repository;
 
-import com.ismataga.to_do_app.entity.ToDo;
+import com.ismataga.to_do_app.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ToDoRepository extends JpaRepository<ToDo,Long> {
+public interface ToDoRepository extends JpaRepository<Task,Long> {
 
 }
