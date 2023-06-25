543525738bc308abd20ae45f9e62ab54c8bed2a7
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-06-25 05:30:17 +0400

    add UserService

---
+++ src/main/java/com/ismataga/to_do_app/service/UserService.java
@@ -0,0 +1,18 @@
+package com.ismataga.to_do_app.service;
+
+import com.ismataga.to_do_app.dto.UserRequest;
+import com.ismataga.to_do_app.dto.UserResponse;
+import com.ismataga.to_do_app.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    void createUser(UserRequest userRequest);
+
+    List<UserResponse> getAllUsers();
+    UserResponse getUserById(Long id);
+
+    void updateUserById(Long id,UserRequest userRequest);
+
+    void deleteUser(Long id);
+}
