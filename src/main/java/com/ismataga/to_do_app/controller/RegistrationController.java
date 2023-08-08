61eb91b9b7c4c08c38a43bbea9151d247702aa8a
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-08-08 14:22:53 +0400

    Add registration controller

--- src/main/java/com/ismataga/to_do_app/controller/RegistrationController.java
+++ src/main/java/com/ismataga/to_do_app/controller/RegistrationController.java
@@ -2,27 +2,41 @@ package com.ismataga.to_do_app.controller;
 
 import com.ismataga.to_do_app.dto.RegistrationDTO;
 import com.ismataga.to_do_app.service.impl.RegistrationService;
+import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/registration")
-public class RegistrationController {
+public class     RegistrationController {
 
     private final RegistrationService registrationService;
 
+
+    @PreAuthorize("permitAll()")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void register(@Valid @RequestBody RegistrationDTO dto){
 
         registrationService.register(dto);
+
+    }
+
+    @GetMapping("/confirmation/{uuid}")
+    public void confirmation(@PathVariable UUID uuid){
 
     }
 }
