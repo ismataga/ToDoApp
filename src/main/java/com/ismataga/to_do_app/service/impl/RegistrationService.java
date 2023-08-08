454365f0d2d7c22531950e7af5f628da00e82ac8
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-08-08 14:24:03 +0400

    Add registrationService

--- src/main/java/com/ismataga/to_do_app/service/impl/RegistrationService.java
+++ src/main/java/com/ismataga/to_do_app/service/impl/RegistrationService.java
@@ -6,8 +6,12 @@ import com.ismataga.to_do_app.entity.User;
 import com.ismataga.to_do_app.mapper.UserMapper;
 import com.ismataga.to_do_app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +19,21 @@ public class RegistrationService {
 
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final MailSender mailSender;
+    @Value("${app.registration.base-path")
+    private String baseUrl;
 
 
+    public void register(RegistrationDTO dto) {
 
-    public void register(RegistrationDTO dto){
-
-        User entity = mapper.toEntity(dto);
-
-
+        User user = Optional.of(dto).map(mapper::toEntity).map(repository::save).orElseThrow();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(dto.getUsername());
+        message.setFrom("info@good.company");
+        message.setSubject("Registration Confirmation");
+        message.setText(baseUrl + "/registration/confirmation" +user.getUuid());
+//        mailSender.send(message);
+        System.out.println(baseUrl + "/registration/confirmation/" +user.getUuid());
     }
 
 
